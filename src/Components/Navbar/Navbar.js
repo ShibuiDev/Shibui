2b9e92e74e383d610d6f318e9a7b95256a9fbfc6
Juan Felipe Mejia Agudelo <juanfelipemejiaagudelo@MacBook-Pro-de-Juan.local>
2022-04-14 16:10:49 -0500

    uploaded

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,12 +1,14 @@
 import React, {useState, useEffect} from 'react'
 import {Container, Wrapper, LogoContainer, Menu, MenuItem, MenuItemLink, MobileIcon} from './Navbar.Elements'
 import { FaBars } from "react-icons/fa";
-import { IoMdClose } from "react-icons/io"
+import { IoMdClose } from 'react-icons/io';
+
 const NavBar = ()=>{
     const [nav, setNav] = useState(false)
 
     const handleClick = ()=>{
         setNav(!nav)
+
     }
  
     return(
