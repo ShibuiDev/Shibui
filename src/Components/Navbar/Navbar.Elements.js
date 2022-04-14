dc6871a17d887634ea1a2f3836d04a990da52658
Juan Felipe Mejia Agudelo <juanfelipemejiaagudelo@MacBook-Pro-de-Juan.local>
2022-04-14 18:08:18 -0500

    uploaded

--- src/Components/Navbar/Navbar.Elements.js
+++ src/Components/Navbar/Navbar.Elements.js
@@ -46,7 +46,7 @@ export const Container = styled.div`
 
 export const Wrapper = styled.div`
     width: 100%;
-    max-width: 1300px;
+    max-width: 100vh;
     height: 100%;
     display: flex;
     flex-wrap: wrap;
